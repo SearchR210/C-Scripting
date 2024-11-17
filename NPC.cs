f0c0708732cacefe214a9c145fcedeeff5777ac9
SearchR210 <168347656+SearchR210@users.noreply.github.com>
2024-11-17 13:05:54 +0300

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Здоровье NPC
+    public int health = 5;
+    // Уровень NPC
+    public int level = 1;
+    // Скорость NPC
+    public float speed = 1.2f;
+    public Vector3 newPosition;
+    void Start()
+    {
+        // Добавляем к здоровь NPC его уровень и выводим здоровье в консоль
+        health += level;
+        print("Здоровье: "+health);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newPosition.z += speed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+}
